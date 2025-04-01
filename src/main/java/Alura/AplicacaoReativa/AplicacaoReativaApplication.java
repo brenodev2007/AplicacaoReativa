06bf9fab7b319b5194f46284ab39e35cc7e71f7d
Breno <breno.soriani@gmail.com>
2025-04-01 17:35:12 -0300

    Initial commit

---
+++ src/main/java/Alura/AplicacaoReativa/AplicacaoReativaApplication.java
@@ -0,0 +1,13 @@
+package Alura.AplicacaoReativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoReativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoReativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Alura/AplicacaoReativa/AplicacaoReativaApplicationTests.java
@@ -0,0 +1,13 @@
+package Alura.AplicacaoReativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacaoReativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
